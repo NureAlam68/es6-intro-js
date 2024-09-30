9a559ecb1e6f41e4675cebb17e6b77768fa992ce
NureAlam68 <nurealam151068@gmail.com>
2024-09-30 14:01:05 +0600

    task four

--- task-four.js
+++ task-four.js
@@ -8,3 +8,14 @@
     3: Find the maximum number from the new array and return the new result
 */
 
+
+const array1 = [1, 2, 30, 4, 5];
+const array2 = [6, 97, 8, 9];
+
+const maxNumber = (x, y) => {
+    const combineArray = [...x, ...y];
+    const maxNum = Math.max(...combineArray);
+    return maxNum;
+}
+const result = maxNumber(array1, array2);
+console.log(result);
